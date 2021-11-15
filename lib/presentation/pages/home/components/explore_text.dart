b3a1b4a876c075db0bcb313e27eb4f18fe47b160
Djihanegh <djihaneghilani@gmail.com>
2021-11-15 23:53:19 +0100

    finished the home screen

--- lib/presentation/pages/home/components/explore_text.dart
+++ lib/presentation/pages/home/components/explore_text.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:travel_app/utils/constants.dart';
+import 'package:travel_app/utils/size_config.dart';
 
 class ExploreText extends StatelessWidget {
   const ExploreText({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-       /* Text(
+    return /* Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [*/
+        /* Text(
           "Explore",
           style: TextStyle(
             fontFamily: 'Muli',
@@ -18,7 +19,7 @@ class ExploreText extends StatelessWidget {
             fontSize: 35,
           ),
         ),*/
-        Spacer(),
+        //Spacer(),
         /* Container(
           height: 50,
           decoration: BoxDecoration(boxShadow: [
@@ -36,27 +37,24 @@ class ExploreText extends StatelessWidget {
         Padding(
             padding: EdgeInsets.only(right: 0),
             child: Container(
+                decoration: BoxDecoration(
+                    color: KGrey, borderRadius: BorderRadius.circular(15)),
                 height: 50,
-                //width: double.infinity,
+                width: SizeConfig.screenWidth! * 1.5,
+                padding: EdgeInsets.all(10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     //Flexible(child: CustomText()),
+                    Text("Search places"),
 
-                    Container(
-                        decoration: BoxDecoration(
-                            color: KGrey,
-                            borderRadius: BorderRadius.circular(15)),
-                        height: 40,
-                        width: 40,
-                        padding: EdgeInsets.all(10),
-                        child: SvgPicture.asset(
-                          "assets/icons/search.svg",
-                          color: Colors.black,
-                        )),
+                    SvgPicture.asset(
+                      "assets/icons/search.svg",
+                      color: Colors.black,
+                    ),
                   ],
-                ))),
-      ],
-    );
+                )));
+    // ],
+    //);
   }
 }
